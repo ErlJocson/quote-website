495b75332479f22339bc650e1ece5170ef966b71
ErlJocson <jocsonerl@gmail.com>
2022-04-18 23:14:47 +0800

    add some designs

--- src/App.js
+++ src/App.js
@@ -19,8 +19,7 @@ function App() {
     getDataInAPI();
   }, []);
 
-  console.log(quote);
-  console.log(author);
+  console.log("https://github.com/skolakoda/programming-quotes-api");
 
   return (
     <div className="container">
